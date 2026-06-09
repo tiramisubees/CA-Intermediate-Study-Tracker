2b3b1a2be908cc18aef59059db25cd2139bab22d
Ruby Banu <rubinam17282005@gmail.com>
2026-06-09 14:19:08 +0530

    completed syllabus structure

--- app/syllabus.py
+++ app/syllabus.py
@@ -1,1 +1,304 @@
-SYLLABUS = {}
+SYLLABUS = {
+
+    "Group 1": {
+
+        "Advanced Accounting": {
+            "Chapters": [
+
+                "Introduction to Accounting Standards",
+
+                "Framework for Preparation and Presentation of Financial Statements",
+
+                "Applicability of Accounting Standards",
+
+                "AS 2",
+                "AS 4",
+                "AS 5",
+                "AS 7",
+                "AS 9",
+                "AS 10",
+
+                "AS 11",
+                "AS 12",
+                "AS 13",
+                "AS 15",
+                "AS 16",
+                "AS 17",
+
+                "AS 18",
+                "AS 19",
+                "AS 20",
+                "AS 22",
+                "AS 24",
+                "AS 25",
+                "AS 26",
+                "AS 28",
+                "AS 29",
+
+                "AS 3",
+
+                "AS 21",
+                "AS 23",
+                "AS 27",
+
+                "Financial Statements of Companies",
+
+                "Buyback of Securities",
+
+                "Amalgamation of Companies",
+
+                "Accounting for Reconstruction of Companies",
+
+                "Accounting for Branches Including Foreign Branches"
+            ]
+        },
+
+        "Corporate and Other Laws": {
+            "Chapters": [
+
+                "Preliminary",
+
+                "Incorporation of Company and Matters Incidental Thereto",
+
+                "Prospectus and Allotment of Securities",
+
+                "Share Capital and Debentures",
+
+                "Acceptance of Deposits by Companies",
+
+                "Registration of Charges",
+
+                "Management and Administration",
+
+                "Declaration and Payment of Dividend",
+
+                "Accounts of Companies",
+
+                "Audit and Auditors",
+
+                "Companies Incorporated Outside India",
+
+                "Limited Liability Partnership Act, 2008",
+
+                "General Clauses Act, 1897",
+
+                "Interpretation of Statutes",
+
+                "Foreign Exchange Management Act, 1999"
+            ]
+        },
+
+        "Taxation": {
+
+            "Direct Tax": [
+
+                "Basic Concepts",
+
+                "Residence and Scope of Total Income",
+
+                "Salaries",
+
+                "Profits and Gains from Business or Profession",
+
+                "Capital Gains",
+
+                "Income from Other Sources",
+
+                "Clubbing of Income",
+
+                "Set-off and Carry Forward of Losses",
+
+                "Deductions from Gross Total Income",
+
+                "Advance Tax, Tax Deduction at Source and Tax Collection at Source",
+
+                "Return of Income",
+
+                "Computation of Total Income"
+            ],
+
+            "Indirect Tax": [
+
+                "GST Introduction",
+
+                "Supply under GST",
+
+                "Charge of GST",
+
+                "Place of Supply",
+
+                "Exemption from GST",
+
+                "Time of Supply",
+
+                "Value of Supply",
+
+                "Input Tax Credit",
+
+                "Registration",
+
+                "Tax Invoice, Credit and Debit Notes",
+
+                "Accounts and Records",
+
+                "E-Way Bill",
+
+                "Payment of Tax",
+
+                "Tax Deduction at Source and Tax Collection at Source under GST",
+
+                "Returns"
+            ]
+        }
+    },
+
+    "Group 2": {
+
+        "Cost and Management Accounting": {
+            "Chapters": [
+
+                "Introduction to Cost and Management Accounting",
+
+                "Material Cost",
+
+                "Employee Cost and Direct Expenses",
+
+                "Overheads - Absorption Costing Method",
+
+                "Activity Based Costing",
+
+                "Cost Sheet",
+
+                "Cost Accounting Systems",
+
+                "Unit and Batch Costing",
+
+                "Job Costing",
+
+                "Process and Operation Costing",
+
+                "Joint Products and By-products",
+
+                "Service Costing",
+
+                "Standard Costing",
+
+                "Marginal Costing",
+
+                "Budgets and Budgetary Control"
+            ]
+        },
+
+        "Auditing and Ethics": {
+            "Chapters": [
+
+                "SA 200",
+
+                "SA 210",
+
+                "SA 300",
+
+                "SA 315",
+
+                "SA 320",
+
+                "SA 330",
+
+                "Automated Environment",
+
+                "SA 500",
+
+                "SA 501",
+
+                "SA 505",
+
+                "SA 510",
+
+                "SA 520",
+
+                "SA 530",
+
+                "Audit of Items of Financial Statements",
+
+                "SA 230",
+
+                "SA 450",
+
+                "SA 550",
+
+                "SA 560",
+
+                "SA 570",
+
+                "SA 580",
+
+                "SA 700",
+
+                "SA 701",
+
+                "SA 705",
+
+                "SA 706",
+
+                "SA 710",
+
+                "SA 299",
+
+                "SA 600",
+
+                "SA 610",
+
+                "SA 260",
+
+                "SA 265",
+
+                "Special Features of Audit of Different Type of Entities",
+
+                "Audit of Banks",
+
+                "Ethics",
+
+                "SA 220",
+
+                "SQC 1"
+            ]
+        },
+
+        "Financial Management and Strategic Management": {
+
+            "Financial Management": [
+
+                "Scope and Objectives of Financial Management",
+
+                "Types of Financing",
+
+                "Financial Analysis and Planning",
+
+                "Cost of Capital",
+
+                "Capital Structure Decisions",
+
+                "Leverages",
+
+                "Investment Decisions",
+
+                "Dividend Decisions",
+
+                "Management of Working Capital"
+            ],
+
+            "Strategic Management": [
+
+                "Introduction to Strategic Management",
+
+                "Strategic Analysis: External Environment",
+
+                "Strategic Analysis: Internal Environment",
+
+                "Strategic Choices",
+
+                "Strategy Implementation and Evaluation"
+            ]
+        }
+    }
+}
